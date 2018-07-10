0e052f150a518d61605cf2fdd4920f852c9cca30
Adham Muhammad <admuaq@gmail.com>
2018-07-10 13:21:33 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   # Change string into Array
-  myArray = sentence1.split(" ").collect do |word| #takes sentence converts it to an array with each element created at " ". Collect method used returns a value of the changes to array 
+  myArray = sentence1.split(" ").collect do |word| #takes sentence converts it to an array with each element created at " ". Collect method used returns a value of the changes to array.
     word.reverse
   end 
   myArray.join(" ")
