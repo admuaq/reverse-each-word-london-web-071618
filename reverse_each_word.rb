e8fc17dcd227d071b76ce4985df87ea1d687d665
Adham Muhammad <admuaq@gmail.com>
2018-07-10 13:17:33 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word(sentence1)
   # Change string into Array
-  myArray = sentence1.split
-  
-  myArray.each do |element|
-    puts element.reverse
+  myArray = sentence1.split(" ").each do |word|
+    word.reverse_each_word
   end 
 
 end
