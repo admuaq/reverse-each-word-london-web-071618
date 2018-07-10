f2c4099dbb183396d447fb89bb1d9b6303c3ffed
Adham Muhammad <admuaq@gmail.com>
2018-07-10 13:22:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(sentence1)
   myArray = sentence1.split(" ").collect do |word| #takes sentence converts it to an array with each element created at " ". Collect method used returns a value of the changes to array.
     word.reverse
   end 
-  myArray.join(" ")
+  myArray.join(" ") #returns array to string with space " " between each original element of the array.
 end
