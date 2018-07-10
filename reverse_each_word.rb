33518c4a3973467b120a189dafd64ed86ae4a434
Adham Muhammad <admuaq@gmail.com>
2018-07-10 13:11:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   # Change string into Array
   myArray = sentence1.split
   
-  myArray.collect do |element|
+  myArray. do |element|
     element.reverse
   end 
 
